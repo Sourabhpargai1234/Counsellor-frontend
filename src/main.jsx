9ac63450e16f105b1fdd5ec411bd7560a3d9d056
Sourabh <Sourabhpar@gmail.com>
2024-06-18 15:17:13 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 import { Provider } from 'react-redux';
-import Store from './Redux/Store.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
