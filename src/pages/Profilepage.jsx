b66d66056b7ee40628a48191f22319adeae1728f
Sourabh <Sourabhpar@gmail.com>
2024-06-21 19:23:34 +0530

    final

--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -44,23 +44,23 @@ export default function ProfilePage() {
       {userData ? (
         <div className="max-w-2xl bg-white shadow-lg rounded-lg overflow-hidden">
           {/* Profile Background Picture */}
-          <img src={userData.coverImage} alt="No coverImage added" className="text-center w-full h-64 object-cover object-center" />
+          <img src={userData.data.coverImage} alt="No coverImage added" className="text-center w-full h-64 object-cover object-center" />
 
           <div className="flex items-center p-6">
             {/* Profile Picture */}
             <img
-              src={userData.avatar}
+              src={userData.data.avatar}
               alt="No Image added"
               className="text-center w-32 h-32 rounded-full object-cover border-4 border-white -mt-16"
             />
 
             <div className="ml-6">
               {/* Username */}
-              <h2 className="text-3xl font-bold mb-2">{userData.username}</h2>
+              <h2 className="text-3xl font-bold mb-2">{userData.data.username}</h2>
               {/* Email */}
-              <p className="text-gray-700 mb-4">{userData.email}</p>
+              <p className="text-gray-700 mb-4">{userData.data.email}</p>
               {/* Bio */}
-              <p className="text-gray-700">{userData.fullName}</p>
+              <p className="text-gray-700">{userData.data.fullName}</p>
               {/* Display other user details as needed */}
             </div>
           </div>
